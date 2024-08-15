165cb7e19250e7bc54f00961540550ff4794a882
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-15 18:59:25 +0800

    feat: Add industry-promotion in landing page
    feat: Install and set up bootstrap-icons

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,13 +1,22 @@
 <script>
 import TheHeader from "@/components/TheHeader.vue";
+import IndustryPromotion from '@/components/index/IndustryPromotion.vue'
 
 export default {
   components: {
     TheHeader,
+    IndustryPromotion
   },
 };
 </script>
 
 <template>
   <TheHeader />
+  <!-- 產業優惠訊息 -->
+  <IndustryPromotion></IndustryPromotion>
 </template>
+<style>
+#app {
+  overflow-x: hidden;
+}
+</style>
---
+++ src/components/index/IndustryPromotion.vue
@@ -0,0 +1,191 @@
+<template>
+  <section id="industry-promotion" class="container mt-16">
+    <h2 class="text-navy04 fs-xl6 fw-bold text-center mb-10">
+      {{ $t("pages.index.industry promotion") }}
+    </h2>
+    <!-- Desktop Carousel (md and above) -->
+    <div
+      id="carouselDesktop"
+      class="carousel slide d-none d-md-block"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          v-for="(chunk, index) in desktopPromotions"
+          :key="'desktop-' + index"
+          class="carousel-item"
+          :class="{ active: index === 0 }"
+        >
+          <ul class="row justify-content-center">
+            <li
+              v-for="(item, i) in chunk"
+              :key="i"
+              class="col-12 col-md-4 mb-6"
+            >
+              <a
+                :href="item.link_url"
+                target="_blank"
+                class="d-block text-center"
+              >
+                <div class="object-fit-container mx-auto">
+                  <img :src="item.preview_url" alt="industryPromotions" />
+                </div>
+                <p class="text-navy04 fw-bold mt-5">{{ item.title }}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev custom-carousel-control"
+        type="button"
+        data-bs-target="#carouselDesktop"
+        data-bs-slide="prev"
+      >
+        <i
+          class="bi bi-arrow-left fs-xl5 text-navy03 carousel-control-prev-icon"
+        ></i>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next custom-carousel-control"
+        type="button"
+        data-bs-target="#carouselDesktop"
+        data-bs-slide="next"
+      >
+        <i
+          class="bi bi-arrow-right fs-xl5 text-navy03 carousel-control-next-icon"
+        ></i>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <!-- Mobile Carousel (below md) -->
+    <div
+      id="carouselMobile"
+      class="carousel slide d-block d-md-none"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          v-for="(item, index) in mobilePromotions"
+          :key="'mobile-' + index"
+          class="carousel-item"
+          :class="{ active: index === 0 }"
+        >
+          <ul class="row justify-content-center">
+            <li :key="index" class="col-12 col-md-6 mb-6">
+              <a
+                :href="item[0].link_url"
+                target="_blank"
+                class="d-block text-center"
+              >
+                <div class="object-fit-container mx-auto">
+                  <img :src="item[0].preview_url" alt="industryPromotions" />
+                </div>
+                <p class="text-navy04 fw-bold mt-5">{{ item[0].title }}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselMobile"
+        data-bs-slide="prev"
+      >
+        <i
+          class="bi bi-arrow-left fs-xl5 text-navy03 carousel-control-prev-icon"
+        ></i>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselMobile"
+        data-bs-slide="next"
+      >
+        <i
+          class="bi bi-arrow-right fs-xl5 text-navy03 carousel-control-next-icon"
+        ></i>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </section>
+</template>
+<style scoped>
+.object-fit-container {
+  width: 100%;
+  max-width: 392px;
+  height: 264px;
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+  }
+}
+.carousel-control-prev {
+  position: absolute;
+  left: -5%;
+  top: 40%;
+  transform: translateY(-50%);
+}
+
+.carousel-control-next {
+  position: absolute;
+  right: -7%;
+  top: 40%;
+  transform: translateY(-50%);
+}
+
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  width: 48px;
+  height: 48px;
+  background: white;
+  border-radius: 50%;
+}
+
+.carousel-inner {
+  position: relative;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      industryPromotions: [],
+    };
+  },
+  methods: {
+    getIndustryPromotions() {
+      const getIndustryPromotionsApi = `api/offer/?row=6&page=1`;
+      this.axios.get(getIndustryPromotionsApi).then((response) => {
+        if (response.data.status == true) {
+          this.industryPromotions = response.data.offers;
+        }
+      });
+    },
+    chunkArray(array, chunkSize) {
+      const chunks = [];
+      for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+      }
+      return chunks;
+    },
+  },
+  computed: {
+    desktopPromotions() {
+      return this.chunkArray(this.industryPromotions.slice(0, 6), 3);
+    },
+    mobilePromotions() {
+      return this.chunkArray(this.industryPromotions.slice(0, 6), 1);
+    },
+  },
+  mounted() {
+    this.getIndustryPromotions();
+  },
+};
+</script>
