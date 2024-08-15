cba9bca4997776f5d8e84c00ce66142740bb7c01
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-15 18:56:50 +0800

    feat: Add proxy api

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://t01.mitwit-cre.com.tw/sub/cli/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     vue(),
     vueDevTools(),
