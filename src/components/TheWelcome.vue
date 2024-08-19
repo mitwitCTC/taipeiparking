88b808e4a5a9b8bf3fb5ac2f757f3cfd20368d7d
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-19 13:08:54 +0800

    feat: Add temporary in landing page

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,14 +2,16 @@
 import TheLayout from "@/components/TheLayout.vue";
 import IndustryPromotion from "@/components/index/IndustryPromotion.vue";
 import TheAnnouncements from "@/components/index/TheAnnouncements.vue";
-import PayBill from "@/components/index/PayBill.vue"
+import PayBill from "@/components/index/PayBill.vue";
+import TheTemporary from "@/components/index/TheTemporary.vue";
 
 export default {
   components: {
     TheLayout,
     IndustryPromotion,
     TheAnnouncements,
-    PayBill
+    PayBill,
+    TheTemporary,
   },
 };
 </script>
@@ -22,6 +24,8 @@ export default {
     <TheAnnouncements></TheAnnouncements>
     <!-- 繳費 -->
     <PayBill></PayBill>
+    <!-- 臨停 -->
+    <TheTemporary></TheTemporary>
   </TheLayout>
 </template>
 <style>
---
+++ src/components/index/TheTemporary.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="temporary">
+    <div class="temporay-bg-container text-center align-content-center">
+      <button class="btn btn-navy03 btn-go-temporary text-xl fw-bold">
+        {{ $t("pages.index.temporary.btn-text") }}
+      </button>
+      <!-- <img src="/index/temporary.png" class="temporay-bg" alt="temporay-bg" /> -->
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.temporay-bg-container {
+  background-image: url("/index/temporary.png");
+  width: 100%;
+  height: 952px;
+}
+
+.btn-go-temporary {
+  padding: 24px 120px;
+}
+
+@media (max-width: 560px) {
+  .temporay-bg-container {
+    height: 720px;
+  }
+  .btn-go-temporary {
+    padding: 12px 48px;
+  }
+}
+
+@media (max-width: 360px) {
+  .temporay-bg-container {
+    height: 640px;
+  }
+}
+</style>
