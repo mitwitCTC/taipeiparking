85211b82afbc7bfc908226c2c82a9e365ce82e40
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-21 09:49:53 +0800

    feat: Adjust file paths for activities images

--- src/components/index/TheActivities.vue
+++ src/components/index/TheActivities.vue
@@ -7,31 +7,31 @@
       <div class="activity-list d-flex gap-4">
         <div class="activity-item">
           <div class="img-container">
-            <img src="/public/index/activities/img01.jpg" />
+            <img src="/index/activities/img01.jpg" />
           </div>
           <p class="text-gray13 fw-bold mt-4">第21屆第1次理監事暨會員聯誼</p>
         </div>
         <div class="activity-item">
           <div class="img-container">
-            <img src="/public/index/activities/img02.jpg" />
+            <img src="/index/activities/img02.jpg" />
           </div>
           <p class="text-gray13 fw-bold mt-4">第21屆第2次理監事暨會員聯誼</p>
         </div>
         <div class="activity-item">
           <div class="img-container">
-            <img src="/public/index/activities/img03.jpg" />
+            <img src="/index/activities/img03.jpg" />
           </div>
           <p class="text-gray13 fw-bold mt-4">第1屆第3次理監事聯席會議</p>
         </div>
         <div class="activity-item">
           <div class="img-container">
-            <img src="/public/index/activities/img04.jpg" />
+            <img src="/index/activities/img04.jpg" />
           </div>
           <p class="text-gray13 fw-bold mt-4">第1屆第4次理監事聯席會議</p>
         </div>
         <div class="activity-item me-4">
           <div class="img-container">
-            <img src="/public/index/activities/img05.jpg" />
+            <img src="/index/activities/img05.jpg" />
           </div>
           <p class="text-gray13 fw-bold mt-4">第1屆第5次理監事聯席會議</p>
         </div>
