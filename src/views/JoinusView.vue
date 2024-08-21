a7829727f7262d4ddea4128cc4ce8e0a80289d2d
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-21 15:39:13 +0800

    feat: Add joinUs page

--- src/views/JoinusView.vue
+++ src/views/JoinusView.vue
@@ -1,11 +1,85 @@
 <template>
   <TheLayout>
-    <div class="container">
-      <p>{{ $t("components.header.joinUs") }}</p>
-      <p>建置中...</p>
-    </div>
+    <section id="join-us" class="join-us bg-yellow03">
+      <div class="container">
+        <p class="text-navy05 fs-xl5 fw-bold mb-8 mb-md-16">
+          {{ $t("pages.joinUs.title") }}
+        </p>
+        <div class="d-flex justify-content-between row gap-1">
+          <div class="method bg-white p-10 col-12 col-md-5 mb-4">
+            <p class="text-navy01 fs-xl3 fw-bold">
+              {{ $t("pages.joinUs.method1.title") }}
+            </p>
+            <p class="text-navy05 fs-xl fw-bold">
+              {{ $t("pages.joinUs.method1.subTitle") }}
+            </p>
+            <hr />
+            <p class="text-gray15 fw-bold">
+              {{ $t("pages.joinUs.method1.downloadForm") }}
+            </p>
+            <p class="text-gray15 formatted-text">
+              {{ $t("pages.joinUs.method1.attachments") }}
+            </p>
+            <p class="text-gray15 fw-bold">
+              {{ $t("pages.joinUs.method1.payment") }}
+            </p>
+            <p class="formatted-text">
+              {{ $t("pages.joinUs.method1.sendDocuments") }}
+            </p>
+          </div>
+          <div class="method bg-white p-10 col-12 col-md-5 mb-4">
+            <p class="text-navy01 fs-xl3 fw-bold">
+              {{ $t("pages.joinUs.method2.title") }}
+            </p>
+            <p class="text-navy05 fs-xl fw-bold">
+              {{ $t("pages.joinUs.method2.subTitle") }}
+            </p>
+            <hr />
+            <p class="text-gray15 fw-bold">
+              {{ $t("pages.joinUs.method2.address") }}
+            </p>
+            <p class="text-gray15 formatted-text">
+              {{ $t("pages.joinUs.method2.attachments") }}
+            </p>
+            <p class="text-gray15 fw-bold">
+              {{ $t("pages.joinUs.method2.payment") }}
+            </p>
+            <p class="formatted-text">
+              {{ $t("pages.joinUs.method2.sendDocuments") }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </TheLayout>
 </template>
+
+<style scoped>
+.join-us {
+  padding-top: 24px;
+  padding-bottom: 117px;
+}
+
+.method {
+  box-shadow: 0px 0px 10px 0px #0000001a;
+  border-radius: 12px;
+}
+
+.formatted-text {
+  white-space: pre-line; /* 保持換行符 \n 的顯示 */
+}
+
+@media (max-width: 800px) {
+  .join-us {
+    padding-top: 24px;
+    padding-bottom: 124px;
+  }
+  .container {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+}
+</style>
 
 <script>
 import TheLayout from "@/components/TheLayout.vue";
