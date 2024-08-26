6b64d8aae66b07427896b949fae11f363cd6c5bd
Eileen-Qian <yiqianc.14@gmail.com>
2024-08-26 23:30:04 +0800

    feat: Update activites page

--- src/views/ActivitiesView.vue
+++ src/views/ActivitiesView.vue
@@ -7,14 +7,47 @@
         </p>
       </div>
       <div class="container mt-4 mt-md-16">
-        建置中
+        <!-- Masonry grid container -->
+        <div class="masonry-grid">
+          <!-- Loop through activities and render each image -->
+          <div
+            v-for="(activity, index) in activities"
+            :key="index"
+            class="masonry-item"
+          >
+            <img :src="activity.imgUrl" :alt="activity.name" />
+            <p class="mt-2">{{ activity.name }}</p>
+          </div>
+        </div>
       </div>
     </section>
   </TheLayout>
 </template>
 
 <style scoped>
+.masonry-grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+}
 
+.masonry-item {
+  width: calc(50% - 16px); /* Adjust the width of each item */
+  margin-bottom: 16px;
+}
+
+.masonry-item img {
+  width: 100%;
+  height: auto;
+  display: block;
+  border-radius: 12px;
+}
+
+@media (max-width: 600px) {
+  .masonry-item {
+    width: 100%;
+  }
+}
 </style>
 
 <script>
@@ -23,6 +56,7 @@ import TheLayout from "@/components/TheLayout.vue";
 export default {
   data() {
     return {
+      activities: [],
     };
   },
   components: {
@@ -31,6 +65,7 @@ export default {
   mounted() {
     this.adjustPaddingTop();
     window.addEventListener("resize", this.adjustPaddingTop);
+    this.getActivities();
   },
   beforeUnmount() {
     window.removeEventListener("resize", this.adjustPaddingTop);
@@ -44,6 +79,30 @@ export default {
         const memberSection = this.$el.querySelector(".member");
         memberSection.style.paddingTop = `${headerHeight}px`;
       }
+    },
+    getActivities() {
+      this.activities = [
+        {
+          name: "第21屆第1次理監事暨會員聯誼",
+          imgUrl: "/index/activities/img01.jpg",
+        },
+        {
+          name: "第21屆第1次理監事暨會員聯誼",
+          imgUrl: "/index/activities/img02.jpg",
+        },
+        {
+          name: "第21屆第1次理監事暨會員聯誼",
+          imgUrl: "/index/activities/img03.jpg",
+        },
+        {
+          name: "第21屆第1次理監事暨會員聯誼",
+          imgUrl: "/index/activities/img04.jpg",
+        },
+        {
+          name: "第21屆第1次理監事暨會員聯誼",
+          imgUrl: "/index/activities/img05.jpg",
+        },
+      ];
     },
   },
 };
